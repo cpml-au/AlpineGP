83e63228561f41811054b4a0b1cf167bdb90f00d
Simone Manti <manti998@gmail.com>
2023-04-20 16:36:45 +0200

    Fixed feasibility of bilevel problem.

--- apps/images/elastica_images.py
+++ apps/images/elastica_images.py
@@ -19,7 +19,7 @@ sys.path.append(parent_directory)
 def elastica_img_from_string(config_file: dict, string: str, X_train, y_train, X_val, y_val):
     from stgp_elastica import plot_sol, eval_MSE, eval_fitness
 
-    penalty = {"method": "length", "reg_param": 0.01}
+    penalty = {"method": "length", "reg_param": 0.001}
     # get normalized simplicial complex
     S_1, x = generate_1_D_mesh(num_nodes=11, L=1.)
     S = SimplicialComplex(S_1, x, is_well_centered=True)
@@ -124,7 +124,9 @@ def elastica_img_from_string(config_file: dict, string: str, X_train, y_train, X
         f"train_fit: {eval_fitness(ind, X_train, y_train, toolbox, S, theta_0_all[0], penalty)[0]}")
     print(
         f"MSE: {eval_MSE(ind, X_val, y_val, toolbox, S, theta_0_all[1], tune_EI0=False)}")
+    plot_sol(ind, X_train, y_train, toolbox, S, theta_0_all[0], transform, False)
     plot_sol(ind, X_val, y_val, toolbox, S, theta_0_all[1], transform, False)
+    plot_sol(ind, X_test, y_test, toolbox, S, theta_0_all[2], transform, False)
 
 
 if __name__ == '__main__':
@@ -141,10 +143,12 @@ if __name__ == '__main__':
     # string = "InnD0(SinD0(theta), SquareD0(InvMulD0(SubD0(FL2_EI0, theta), SqrtF(InnP0(int_coch, InvSt0(ExpD1(SquareD1(dD0(theta)))))))))"
     # string = " InnD0(SinD0(theta), SquareD0(InvMulD0(SubD0(FL2_EI0, theta), InnP0(int_coch, InvSt0(ExpD1(SquareD1(dD0(theta))))))))"
     # string = "InnD0(SquareD0(CosD0(AddD0(FL2_EI0, theta))), theta)"
-    string = "InnD0(theta, SubD0(theta, theta))"
+    # string = "Add(SinF(Add(Add(-1, 2), Add(CosF(InnD1(InvMulD1(CosD1(CochMulD1(SinD1(SqrtD1(St0(int_coch))), dD0(theta))), -1), InvMulD1(SubD1(dD0(FL2_EI0), St0(int_coch)), -1))), SqrtF(SinF(2))))), CosF(InnD0(MulD0(AddD0(theta, FL2_EI0), MulF(-1, -1)), ExpD0(theta))))"
     # string = " InnD0(SinD0(SinD0(SubD0(InvMulD0(FL2_EI0, 2), theta))), theta)"
-    # string = "SinF(Sub(ArccosF(InnD0(theta, theta)), InnD0(theta, delD1(SinD1(SubD1(CosD1(MulD1(dD0(theta), 2)), SinD1(dD0(FL2_EI0))))))))"
+    # string = "Add(SinF(SqrtF(Add(ExpF(CosF(InvF(SinF(2)))), Add(CosF(InnD1(CosD1(CochMulD1(SinD1(SqrtD1(St0(int_coch))), dD0(theta))), SubD1(dD0(FL2_EI0), St0(int_coch)))), SqrtF(SinF(2)))))), CosF(InnD0(AddD0(FL2_EI0, theta), ExpD0(InvMulD0(theta, 1/2)))))"
     # string = "Sub(InnP0(InvSt0(dD0(theta), InvSt0(dD0(theta))), InnD0(FL2_EI0, SinD0(theta))"
-    # string = " CosF(InnD0(ExpD0(theta), FL2_EI0))"
+    # string = " SinF(CosF(InnD0(ExpD0(theta), FL2_EI0)))"
+    # string = "InnD0(theta, SubD0(theta, theta))"
+    string = "InnP0(ArccosP0(SubP0(SqrtP0(int_coch), int_coch)), SinP0(InvSt0(dD0(SqrtD0(CosD0(SubD0(FL2_EI0, theta)))))))"
     elastica_img_from_string(config_file, string=string,
                              X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val)
