9739281a11eae46ccc21546c5777828976ca4a41
Simone Manti <manti998@gmail.com>
2023-08-22 15:38:28 +0200

    [WIP] Generated dataset for linear elasticity and reorganized data generation functions.

--- src/alpine/data/poisson/poisson_dataset.py
+++ src/alpine/data/poisson/poisson_dataset.py
@@ -2,8 +2,10 @@ import numpy as np
 from dctkit.mesh import simplex, util
 from dctkit.dec import cochain as C
 import os
-from sklearn.model_selection import train_test_split
 from dctkit import config
+import alpine.data.util as u
+import numpy.typing as npt
+from typing import Tuple
 
 # choose precision and whether to use GPU or CPU
 # needed for context of the plots at the end of the evolution
@@ -14,22 +16,24 @@ cwd = os.path.dirname(simplex.__file__)
 data_path = os.path.dirname(os.path.realpath(__file__))
 
 
-def generate_dataset(S, num_samples_per_source, num_sources, noise):
+def generate_dataset(S: simplex.SimplicialComplex, num_samples_per_source: int,
+                     num_sources: int, noise: npt.NDArray) -> Tuple[npt.NDArray,
+                                                                    npt.NDArray]:
     """Generate a dataset for the Poisson problem Delta u + f = 0, i.e. collection of
     pairs source term - solution vectors.
 
     Args:
-        S (SimplicialComplex): simplicial complex where the functions of the dataset
+        S: simplicial complex where the functions of the dataset
             are defined.
-        num_samples_per_source (int): the multiplicity of every class (for now 3) of
+        num_samples_per_source: the multiplicity of every class (for now 3) of
             functions of the dataset.
-        num_sources (int): number of types (1-3) of functions used to represent the
+        num_sources: number of types (1-3) of functions used to represent the
             source term.
-        noise (np.array): noise to perturb the solution vector.
+        noise: noise to perturb the solution vector.
 
     Returns:
-        np.array: np.array of the dataset samples.
-        np.array: np.array of the labels.
+        np.array of the dataset samples.
+        np.array of the labels.
     """
     node_coords = S.node_coords
     num_nodes = S.num_nodes
@@ -71,93 +75,7 @@ def generate_dataset(S, num_samples_per_source, num_sources, noise):
     return data_X, data_y
 
 
-def split_dataset(X, y, perc_val, perc_test, is_valid=False):
-    """Split the dataset in training, validation and test set (double hold out).
-    Args:
-        X (np.array): samples of the dataset
-        y (np.array): targets of the dataset
-        perc_val (float): percentage of the dataset dedicated to validation set
-        perc_test (float): percentage of the dataset dedicated to validation set
-        is_valid (bool): boolean that it is True if we want to do model selection
-        (validation process).
-
-    Returns:
-        (tuple): tuple of training and test samples.
-        (tuple): tuple of training and test targets.
-    """
-
-    if not is_valid:
-        return X, y
-
-    # split the dataset in training and test set
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=perc_test, random_state=42)
-
-    # split X_train in training and validation set
-
-    X_t, X_valid, y_t, y_valid = train_test_split(
-        X_train, y_train, test_size=perc_val, random_state=42)
-
-    X = (X_t, X_valid, X_test)
-    y = (y_t, y_valid, y_test)
-
-    return X, y
-
-
-def save_dataset(S, num_samples_per_source, num_sources, noise):
-    """Generate, split and save the dataset.
-
-    Args:
-        S (SimplicialComplex): simplicial complex where the functions of the dataset
-        are defined.
-        num_samples_per_source (int): the multiplicity of every class (for now 3) of
-        functions of the dataset.
-        num_sources (int): number of types (1-3) of functions used to represent the
-        source term.
-        different functions in the dataset.
-        noise (np.array): noise to perturb the solution vector.
-    """
-    data_X, data_y = generate_dataset(S, num_samples_per_source, num_sources, noise)
-    X, y = split_dataset(data_X, data_y, 0.25, 0.25, True)
-    X_train, X_valid, X_test = X
-    y_train, y_valid, y_test = y
-    np.savetxt("X_train.csv", X_train, delimiter=",")
-    np.savetxt("X_valid.csv", X_valid, delimiter=",")
-    np.savetxt("X_test.csv", X_test, delimiter=",")
-    np.savetxt("y_train.csv", y_train, delimiter=",")
-    np.savetxt("y_valid.csv", y_valid, delimiter=",")
-    np.savetxt("y_test.csv", y_test, delimiter=",")
-
-
-def load_dataset():
-    """Load the dataset from .csv files.
-
-    Returns:
-        (np.array): training samples.
-        (np.array): validation samples.
-        (np.array): test samples.
-        (np.array): training targets.
-        (np.array): validation targets.
-        (np.array): test targets.
-
-
-    """
-    X_train = np.loadtxt(os.path.join(data_path, "X_train.csv"),
-                         dtype=float, delimiter=",")
-    X_valid = np.loadtxt(os.path.join(data_path, "X_valid.csv"),
-                         dtype=float, delimiter=",")
-    X_test = np.loadtxt(os.path.join(data_path, "X_test.csv"),
-                        dtype=float, delimiter=",")
-    y_train = np.loadtxt(os.path.join(data_path, "y_train.csv"),
-                         dtype=float, delimiter=",")
-    y_valid = np.loadtxt(os.path.join(data_path, "y_valid.csv"),
-                         dtype=float, delimiter=",")
-    y_test = np.loadtxt(os.path.join(data_path, "y_test.csv"),
-                        dtype=float, delimiter=",")
-    return X_train, X_valid, X_test, y_train, y_valid, y_test
-
-
-def save_noise(num_nodes):
+def save_noise(num_nodes: int):
     noise = 1.*np.random.rand(num_nodes)
     np.save("noise_poisson.npy", noise)
 
@@ -167,15 +85,15 @@ def load_noise():
 
 
 if __name__ == '__main__':
-    # seet seed
+    # set seed
     np.random.seed(42)
     mesh, _ = util.generate_square_mesh(0.08)
     S = util.build_complex_from_mesh(mesh)
     S.get_hodge_star()
     num_nodes = S.num_nodes
-    save_dataset(S, 4, 3, 0.*np.random.rand(num_nodes))
-    # save_noise(num_nodes)
-    # data_X, _ = generate_dataset(S, 4, 3, 0.*np.random.rand(num_nodes))
-    # for i in range(12):
-    #    plt.tricontourf(triang, data_X[i, :], cmap='RdBu', levels=20)
-    #    plt.show()
+    data_generator_kwargs = {'S': S, 'num_samples_per_source': 4, 'num_sources': 3,
+                             'noise': 0.*np.random.rand(num_nodes)}
+    u.save_dataset(data_generator=generate_dataset,
+                   data_generator_kwargs=data_generator_kwargs,
+                   perc_val=0.25,
+                   perc_test=0.25)
