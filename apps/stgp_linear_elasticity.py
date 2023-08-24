85778c6d9f215c940de79681ca17a29f6c8bd206
Simone Manti <manti998@gmail.com>
2023-08-24 13:55:11 +0200

    [WIP] Fixed util.py in data. Debugging stgp_linear_elasticity

--- apps/stgp_linear_elasticity.py
+++ apps/stgp_linear_elasticity.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib import tri
 from deap import gp, base
 from alpine.data.util import load_dataset
+from alpine.data.linear_elasticity.linear_elasticity_dataset import data_path
 from dctkit.mesh import util
 from alpine.gp import gpsymbreg as gps
 from dctkit import config
@@ -28,12 +29,10 @@ residual_formulation = False
 # needed for context of the plots at the end of the evolution
 config()
 
-noise = pd.load_noise()
 
-
-def deformation_gradient(c: C.CochainP0) -> C.CochainD0:
+def deformation_gradient(c: C.CochainP0) -> C.CochainP2:
     F = c.complex.get_deformation_gradient(c.coeffs)
-    return C.CochainD0(c.complex, F)
+    return C.CochainP2(c.complex, F)
 
 
 def transpose(c: C.Cochain) -> C.Cochain:
@@ -44,6 +43,25 @@ def trace(c: C.Cochain) -> C.Cochain:
     return C.Cochain(c.complex, jnp.trace(c.coeffs, axis1=1, axis2=2))
 
 
+def get_boundary_values(X, left_bnd_nodes_idx, left_bnd_nodes_pos, right_bnd_nodes_idx,
+                        right_bnd_pos_components, bottom_left_corner,
+                        bottom_left_corner_pos):
+    bvalues_X = []
+    for true_curr_node_coords in X:
+        right_bnd_nodes_pos = true_curr_node_coords[right_bnd_nodes_idx,
+                                                    :][:, right_bnd_pos_components]
+
+        # NOTE: without flatten it does not work properly when concatenating multiple bcs;
+        # fix this so that flatten is not needed (not intuitive)
+        boundary_values = {"0": (left_bnd_nodes_idx + right_bnd_nodes_idx,
+                                 np.vstack((left_bnd_nodes_pos,
+                                           right_bnd_nodes_pos)).flatten()),
+                           ":": (bottom_left_corner, bottom_left_corner_pos)}
+        bvalues_X.append(boundary_values)
+    return bvalues_X
+
+
+'''
 def is_valid_energy(u: npt.NDArray, prb: oc.OptimizationProblem,
                     bnodes: npt.NDArray) -> bool:
     # perturb solution and check whether the gradient still vanishes
@@ -53,58 +71,65 @@ def is_valid_energy(u: npt.NDArray, prb: oc.OptimizationProblem,
     grad_u_noise = jnp.linalg.norm(prb.gradient(u_noise))
     is_valid = grad_u_noise >= 1e-6
     return is_valid
+'''
 
 
-def eval_MSE_sol(func: Callable, indlen: int, X: npt.NDArray, y: npt.NDArray,
-                 bvalues: dict, S: SimplicialComplex, bnodes: npt.NDArray,
-                 gamma: float, u_0: C.CochainP0) -> Tuple[float, npt.NDArray]:
+def eval_MSE_sol(func: Callable, indlen: int, X: npt.NDArray,
+                 bvalues: dict, S: SimplicialComplex, gamma: float,
+                 u_0: C.CochainP0) -> Tuple[float, npt.NDArray]:
 
-    num_nodes = X.shape[1]
+    num_data, num_nodes, dim_embedded_space = X.shape
 
     # need to call config again before using JAX in energy evaluations to make sure that
     # the current worker has initialized JAX
     config()
 
     # create objective function and set its energy function
-    def total_energy(x, vec_y):
+    def total_energy(x, curr_bvalues):
+        x_reshaped = x.reshape(S.node_coords.shape)
         penalty = 0.
-        for key in bvalues:
-            idx, values = bvalues[key]
+        for key in curr_bvalues:
+            idx, values = curr_bvalues[key]
             if key == ":":
-                penalty += jnp.sum((x[idx, :] - values)**2)
+                penalty += jnp.sum((x_reshaped[idx, :] - values)**2)
             else:
-                penalty += jnp.sum((x[idx, int(key)] - values)**2)
+                penalty += jnp.sum((x_reshaped[idx, int(key)] - values)**2)
         penalty *= 0.5*gamma
-        epsilon = C.CochainD0(S, x)
+        nodes = C.CochainP0(S, x_reshaped)
         # if residual_formulation:
         #    total_energy = C.inner_product(func(c, fk), func(c, fk)) + penalty
         # else:
-        total_energy = func(epsilon, vec_y) + penalty
+        total_energy = func(nodes) + penalty
         return total_energy
 
-    prb = oc.OptimizationProblem(
-        dim=num_nodes, state_dim=num_nodes, objfun=total_energy)
+    prb = oc.OptimizationProblem(dim=num_nodes*dim_embedded_space,
+                                 state_dim=num_nodes*dim_embedded_space,
+                                 objfun=total_energy)
 
     total_err = 0.
 
     best_sols = []
 
-    for i, vec_y in enumerate(y):
+    # for i, x in enumerate(X):
+    for i in range(num_data):
         # extract current bvalues
-        # vec_bvalues = bvalues[i, :]
+        curr_bvalues = bvalues[i]
 
         # set current bvalues and vec_y for the Poisson problem
         # args = {'vec_y': vec_y, 'vec_bvalues': vec_bvalues}
-        args = {'vec_y': vec_y}
+        args = {'curr_bvalues': curr_bvalues}
         prb.set_obj_args(args)
 
         # minimize the objective
-        x = prb.solve(x0=u_0.coeffs, ftol_abs=1e-12, ftol_rel=1e-12, maxeval=1000)
+        x = prb.solve(x0=u_0.coeffs.flatten(), ftol_abs=1e-12,
+                      ftol_rel=1e-12, maxeval=1000)
 
         if (prb.last_opt_result == 1 or prb.last_opt_result == 3
                 or prb.last_opt_result == 4):
             # check whether the energy is "admissible" (i.e. exclude constant energies)
-            valid_energy = is_valid_energy(u=x, prb=prb, bnodes=bnodes)
+            # valid_energy = is_valid_energy(u=x, prb=prb, bnodes=bnodes)
+            # FIXME: get rid of valid_energy
+            valid_energy = True
 
             if valid_energy:
                 current_err = np.linalg.norm(x-X[i, :])**2
@@ -127,33 +152,32 @@ def eval_MSE_sol(func: Callable, indlen: int, X: npt.NDArray, y: npt.NDArray,
 
 
 @ray.remote(num_cpus=2)
-def eval_best_sols(individual: Callable, indlen: int, X: npt.NDArray, y: npt.NDArray,
-                   bvalues: dict, S: SimplicialComplex, bnodes: npt.NDArray,
+def eval_best_sols(individual: Callable, indlen: int, X: npt.NDArray,
+                   bvalues: dict, S: SimplicialComplex,
                    gamma: float, u_0: npt.NDArray, penalty: dict) -> npt.NDArray:
 
-    _, best_sols = eval_MSE_sol(individual, indlen, X, y, bvalues, S, bnodes,
+    _, best_sols = eval_MSE_sol(individual, indlen, X, bvalues, S,
                                 gamma, u_0)
 
     return best_sols
 
 
 @ray.remote(num_cpus=2)
-def eval_MSE(individual: Callable, indlen: int, X: npt.NDArray, y: npt.NDArray,
-             bvalues: dict, S: SimplicialComplex, bnodes: npt.NDArray,
+def eval_MSE(individual: Callable, indlen: int, X: npt.NDArray,
+             bvalues: dict, S: SimplicialComplex,
              gamma: float, u_0: npt.NDArray, penalty: dict) -> float:
 
-    MSE, _ = eval_MSE_sol(individual, indlen, X, y, bvalues, S, bnodes, gamma, u_0)
+    MSE, _ = eval_MSE_sol(individual, indlen, X, bvalues, S, gamma, u_0)
 
     return MSE
 
 
 @ray.remote(num_cpus=2)
-def eval_fitness(individual: Callable, indlen: int, X: npt.NDArray, y: npt.NDArray,
-                 bvalues: dict, S: SimplicialComplex, bnodes: npt.NDArray, gamma: float,
+def eval_fitness(individual: Callable, indlen: int, X: npt.NDArray,
+                 bvalues: dict, S: SimplicialComplex, gamma: float,
                  u_0: npt.NDArray, penalty: dict) -> Tuple[float, ]:
 
-    total_err, _ = eval_MSE_sol(individual, indlen, X, y, bvalues, S, bnodes,
-                                gamma, u_0)
+    total_err, _ = eval_MSE_sol(individual, indlen, X, bvalues, S, gamma, u_0)
 
     # penalty terms on length
     objval = total_err + penalty["reg_param"]*indlen
@@ -206,10 +230,11 @@ def stgp_linear_elasticity(config_file, output_path=None):
     S.get_flat_DPP_weights()
 
     # load data
-    X_train, X_val, X_test, y_train, y_val, y_test = load_dataset()
+    X_train, X_val, X_test, y_train, y_val, y_test = load_dataset(data_path, "npy")
 
     # set bc
     num_nodes = S.num_nodes
+    num_faces = S.S[2].shape[0]
     ref_node_coords = S.node_coords
 
     bnd_edges_idx = S.bnd_faces_indices
@@ -225,31 +250,39 @@ def stgp_linear_elasticity(config_file, output_path=None):
 
     left_bnd_nodes_pos = ref_node_coords[left_bnd_nodes_idx,
                                          :][:, left_bnd_pos_components]
-    right_bnd_nodes_pos = true_curr_node_coords[right_bnd_nodes_idx,
-                                                :][:, right_bnd_pos_components]
     bottom_left_corner_pos = ref_node_coords[bottom_left_corner, :]
 
     # FIXME: just to initialize ref_metric_contravariant. Write a routine in simplex that does it
     _ = S.get_deformation_gradient(ref_node_coords)
 
-    # NOTE: without flatten it does not work properly when concatenating multiple bcs;
-    # fix this so that flatten is not needed (not intuitive)
-    boundary_values = {"0": (left_bnd_nodes_idx + right_bnd_nodes_idx,
-                             np.vstack((left_bnd_nodes_pos,
-                                        right_bnd_nodes_pos)).flatten()),
-                       ":": (bottom_left_corner, bottom_left_corner_pos)}
-
     # extract boundary values
-    bvalues_train = X_train[:, bnodes]
-    bvalues_val = X_val[:, bnodes]
-    bvalues_test = X_test[:, bnodes]
+    bvalues_train = get_boundary_values(X_train,
+                                        left_bnd_nodes_idx,
+                                        left_bnd_nodes_pos,
+                                        right_bnd_nodes_idx,
+                                        right_bnd_pos_components,
+                                        bottom_left_corner,
+                                        bottom_left_corner_pos)
+    bvalues_val = get_boundary_values(X_val,
+                                      left_bnd_nodes_idx,
+                                      left_bnd_nodes_pos,
+                                      right_bnd_nodes_idx,
+                                      right_bnd_pos_components,
+                                      bottom_left_corner,
+                                      bottom_left_corner_pos)
+    bvalues_test = get_boundary_values(X_test,
+                                       left_bnd_nodes_idx,
+                                       left_bnd_nodes_pos,
+                                       right_bnd_nodes_idx,
+                                       right_bnd_pos_components,
+                                       bottom_left_corner,
+                                       bottom_left_corner_pos)
 
     # penalty parameter for the Dirichlet bcs
-    gamma = 1000.
+    gamma = 10000.
 
-    # initial guess for the solution of the Poisson problem
-    u_0_vec = np.zeros(num_nodes, dtype=dctkit.float_dtype)
-    u_0 = C.CochainP0(S, u_0_vec)
+    # initial guess for the solution of the problem
+    u_0 = C.CochainP0(S, ref_node_coords)
 
     residual_formulation = config_file["gp"]["residual_formulation"]
 
@@ -261,15 +294,36 @@ def stgp_linear_elasticity(config_file, output_path=None):
         pset.addTerminal(C.Cochain(S.num_nodes, True, S, np.ones(
             S.num_nodes, dtype=dctkit.float_dtype)), C.Cochain, name="ones")
     else:
-        pset = gp.PrimitiveSetTyped("MAIN", [C.CochainP0, C.CochainP0], float)
+        pset = gp.PrimitiveSetTyped("MAIN", [C.CochainP0], float)
 
     # add constants
     pset.addTerminal(0.5, float, name="1/2")
     pset.addTerminal(-1., float, name="-1.")
     pset.addTerminal(2., float, name="2.")
+    pset.addTerminal(10., float, name="10.")
+    pset.addTerminal(0.1, float, name="0.1")
+
+    identity = jnp.stack([jnp.identity(2)]*num_faces)
+    identity_coch = C.CochainP2(S, identity)
+    pset.addTerminal(identity_coch, C.CochainP2, name="I")
+
+    # add special primitives
+    pset.addPrimitive(deformation_gradient, [C.CochainP0], C.CochainP2, name="def_grad")
+    pset.addPrimitive(trace, [C.CochainP0], C.CochainP0, name="trP0")
+    pset.addPrimitive(trace, [C.CochainP1], C.CochainP1, name="trP1")
+    pset.addPrimitive(trace, [C.CochainP2], C.CochainP2, name="trP2")
+    pset.addPrimitive(trace, [C.CochainD0], C.CochainD0, name="trD0")
+    pset.addPrimitive(trace, [C.CochainD1], C.CochainD1, name="trD1")
+    pset.addPrimitive(trace, [C.CochainD2], C.CochainD2, name="trD2")
+    pset.addPrimitive(transpose, [C.CochainP0], C.CochainP0, name="TP0")
+    pset.addPrimitive(transpose, [C.CochainP1], C.CochainP1, name="TP1")
+    pset.addPrimitive(transpose, [C.CochainP2], C.CochainP2, name="TP2")
+    pset.addPrimitive(transpose, [C.CochainD0], C.CochainD0, name="TD0")
+    pset.addPrimitive(transpose, [C.CochainD1], C.CochainD1, name="TD1")
+    pset.addPrimitive(transpose, [C.CochainD2], C.CochainD2, name="TD2")
+
     # rename arguments
-    pset.renameArguments(ARG0="u")
-    pset.renameArguments(ARG1="f")
+    pset.renameArguments(ARG0="nodes")
 
     # create symbolic regression problem instance
     GPprb = gps.GPSymbRegProblem(pset=pset, config_file_data=config_file)
@@ -277,18 +331,18 @@ def stgp_linear_elasticity(config_file, output_path=None):
     penalty = config_file["gp"]["penalty"]
 
     GPprb.store_eval_common_params({'S': S, 'penalty': penalty,
-                                    'bnodes': bnodes,
                                     'gamma': gamma, 'u_0': u_0})
 
-    params_names = ('X', 'y', 'bvalues')
-    datasets = {'train': [X_train, y_train, bvalues_train],
-                'val': [X_val, y_val, bvalues_val],
-                'test': [X_test, y_test, bvalues_test]}
+    params_names = ('X', 'bvalues')
+    datasets = {'train': [X_train, bvalues_train],
+                'val': [X_val, bvalues_val],
+                'test': [X_test, bvalues_test]}
     GPprb.store_eval_dataset_params(params_names, datasets)
 
     GPprb.register_eval_funcs(fitness=eval_fitness.remote, error_metric=eval_MSE.remote,
                               test_sols=eval_best_sols.remote)
 
+    '''
     if GPprb.plot_best:
         triang = tri.Triangulation(S.node_coords[:, 0], S.node_coords[:, 1], S.S[2])
         GPprb.toolbox.register("plot_best_func", plot_sol, X=X_val, y=y_val,
@@ -296,6 +350,7 @@ def stgp_linear_elasticity(config_file, output_path=None):
                                gamma=gamma, u_0=u_0,
                                toolbox=GPprb.toolbox, triang=triang)
 
+    '''
     GPprb.register_map([len])
 
     start = time.perf_counter()
