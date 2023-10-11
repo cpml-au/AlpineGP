68035c3965280bb6a440601ad00af5e7ed4f2650
Simone Manti <manti998@gmail.com>
2023-10-11 16:39:24 +0200

    Added main documentation in primitives.py

--- src/alpine/util.py
+++ src/alpine/util.py
@@ -59,3 +59,51 @@ def get_positions_from_angles(angles: Tuple) -> Tuple:
         x_all.append(x_i)
         y_all.append(y_i)
     return x_all, y_all
+
+
+def get_LE_boundary_values(X, y, ref_node_coords, boundary_nodes_info):
+    bvalues_X = []
+    right_bnd_nodes_idx = boundary_nodes_info['right_bnd_nodes_idx']
+    left_bnd_nodes_idx = boundary_nodes_info['left_bnd_nodes_idx']
+    up_bnd_nodes_idx = boundary_nodes_info['up_bnd_nodes_idx']
+    down_bnd_nodes_idx = boundary_nodes_info['down_bnd_nodes_idx']
+    for i, data_label in enumerate(y):
+        true_curr_node_coords = X[i, :, :]
+        if data_label == "pure_tension":
+            bot_left_corn_idx = left_bnd_nodes_idx.index(0)
+            bottom_left_corner = left_bnd_nodes_idx[bot_left_corn_idx]
+            left_bnd_nodes_without_corner = left_bnd_nodes_idx[:bot_left_corn_idx] + \
+                left_bnd_nodes_idx[(bot_left_corn_idx+1):]
+
+            left_bnd_pos_components = [0]
+            right_bnd_pos_components = [0]
+
+            left_bnd_nodes_pos = ref_node_coords[left_bnd_nodes_without_corner,
+                                                 :][:, left_bnd_pos_components]
+            bottom_left_corner_pos = ref_node_coords[bottom_left_corner, :]
+            right_bnd_nodes_pos = true_curr_node_coords[right_bnd_nodes_idx,
+                                                        :][:, right_bnd_pos_components]
+
+            # NOTE: without flatten it does not work properly when concatenating
+            # multiple bcs; fix this so that flatten is not needed (not intuitive)
+            boundary_values = {"0": (left_bnd_nodes_without_corner+right_bnd_nodes_idx,
+                                     np.vstack((left_bnd_nodes_pos,
+                                               right_bnd_nodes_pos)).flatten()),
+                               ":": (bottom_left_corner, bottom_left_corner_pos)}
+        elif data_label == "pure_shear":
+            up_bnd_nodes_pos_x = true_curr_node_coords[up_bnd_nodes_idx, 0]
+            up_bnd_nodes_pos_y = ref_node_coords[up_bnd_nodes_idx, 1]
+            up_bnd_pos = np.zeros((len(up_bnd_nodes_idx), 3))
+            up_bnd_pos[:, 0] = up_bnd_nodes_pos_x
+            up_bnd_pos[:, 1] = up_bnd_nodes_pos_y
+            down_bnd_pos = ref_node_coords[down_bnd_nodes_idx, :]
+            left_bnd_pos = true_curr_node_coords[left_bnd_nodes_idx, :]
+            right_bnd_pos = true_curr_node_coords[right_bnd_nodes_idx, :]
+            bnodes = left_bnd_nodes_idx + right_bnd_nodes_idx + \
+                up_bnd_nodes_idx + down_bnd_nodes_idx
+            bvalues = np.vstack((left_bnd_pos, right_bnd_pos,
+                                 up_bnd_pos, down_bnd_pos))
+            boundary_values = {":": (bnodes, bvalues)}
+
+        bvalues_X.append(boundary_values)
+    return bvalues_X
