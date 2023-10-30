268decbf05018de373a068a9356da53ec389096d
Simone Manti <manti998@gmail.com>
2023-10-30 12:02:44 +0100

    Generated dimensionless burgers data.

--- src/alpine/data/burgers/burgers_dataset.py
+++ src/alpine/data/burgers/burgers_dataset.py
@@ -19,25 +19,43 @@ def burgers_data(x_max, t_max, dx, dt, u_0, nodes_BC, epsilon, scheme="parabolic
 
 
 if __name__ == "__main__":
-    x_max = 5
-    t_max = 2
-    dx = 0.025
-    dt = 0.001
-    num_x_points = int(x_max/dx)
-    num_t_points = int(t_max/dt)
-    x = np.linspace(0, x_max, num_x_points)
-    x_circ = (x[:-1] + x[1:])/2
+    # SPACE PARAMS
+    L = 5
+    L_norm = 1
+    # spatial resolution
+    dx = 0.05
+    dx_norm = dx/L
+    #  Number of spatial grid points
+    num_x_points_norm = int(L_norm / dx_norm)
 
-    # boundary and initial conditions
-    u_0 = 2 * np.exp(-2 * (x_circ - 0.5 * x_max)**2)
-    nodes_BC = {'left': np.zeros(num_t_points), 'right': np.zeros(num_t_points)}
+    # vector containing spatial points
+    x_norm = np.linspace(0, L_norm, num_x_points_norm)
+    x_norm_circ = (x_norm[:-1] + x_norm[1:])/2
 
-    # viscosity coefficient
-    epsilon = 0.
+    # initial velocity
+    u_0 = 2 * np.exp(-2 * (x_norm_circ - 0.5 * L)**2)
+    umax = np.max(u_0)
+
+    # TIME PARAMS
+    T = 2
+    T_norm = T*umax/L
+    # temporal resolution
+    dt = 0.01
+    dt_norm = dt*umax/L
+    # number of temporal grid points
+    num_t_points_norm = int(T_norm / dt_norm)
+
+    # Viscosity
+    epsilon = 0.06
+    epsilon_norm = epsilon/(L*umax)
+
+    nodes_BC = {'left': np.zeros(num_t_points_norm),
+                'right': np.zeros(num_t_points_norm)}
+
     u.save_dataset(data_generator=burgers_data,
-                   data_generator_kwargs={'x_max': x_max, 't_max': t_max, 'dx': dx,
-                                          'dt': dt, 'u_0': u_0, 'nodes_BC': nodes_BC,
-                                          'epsilon': epsilon, 'scheme': "upwind"},
+                   data_generator_kwargs={'x_max': L_norm, 't_max': T_norm, 'dx': dx_norm,
+                                          'dt': dt_norm, 'u_0': u_0/umax, 'nodes_BC': nodes_BC,
+                                          'epsilon': epsilon_norm, 'scheme': "parabolic"},
                    perc_val=0.3,
                    perc_test=0.2,
                    format="npy")
